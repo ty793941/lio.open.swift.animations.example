55d2c58a49476d5806a213c7631eccb393ab8353
zhanghai <zhanghai793941@163.com>
2024-12-02 09:42:19 +0800

    Initial Commit

---
+++ lio.swift.example/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  lio.swift.example
+//
+//  Created by 张海 on 2024/12/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ lio.swift.example/lio_swift_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  lio_swift_exampleApp.swift
+//  lio.swift.example
+//
+//  Created by 张海 on 2024/12/2.
+//
+
+import SwiftUI
+
+@main
+struct lio_swift_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
